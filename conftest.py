58eae6770e6fc0b91e8abce94e09242b2fc8ce49
miratava <miratava@gmail.com>
2021-09-07 09:21:25 +0200

    Added headless options for chrome and firefox

--- conftest.py
+++ conftest.py
@@ -1,15 +1,29 @@
 # conftest.py
 import pytest
 from selenium import webdriver
+from selenium.webdriver import chrome
+from selenium.webdriver import firefox
+
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", help="input browser (chrome/firefoxf)")
-   
+    
+
+def get_chrome_options():
+    options = chrome.Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')  
+    return options
+
+def get_firefox_option():
+    options = firefox.options.Options()
+    options.headless = True
+    return options
 
 def get_driver(input_params):
-        browsers = {"firefoxf": lambda : webdriver.Firefox(), "chrome": lambda : webdriver.Chrome()}
-        driver = browsers[input_params['browser']]()
-        return driver
+    browsers = {"firefoxf": lambda : webdriver.Firefox(options=get_firefox_option()), "chrome": lambda : webdriver.Chrome(chrome_options=get_chrome_options())}
+    driver = browsers[input_params['browser']]()
+    return driver
 
 
 @pytest.fixture

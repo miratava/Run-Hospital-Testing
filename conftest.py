e4f67eb3364dc8a8e2530756dba2e542e557436f
miratava <miratava@gmail.com>
2021-09-07 09:32:35 +0200

    A bug in confest.py was fixed

--- conftest.py
+++ conftest.py
@@ -10,7 +10,7 @@ def pytest_addoption(parser):
     
 
 def get_chrome_options():
-    options = chrome.Options()
+    options = chrome.option.Options()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')  
     return options
